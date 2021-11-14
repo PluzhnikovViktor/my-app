e90287154971a517f7128982d61ca25316829eb1
PluzhnikovViktor <vi.plujnikov@it-justice.com>
2021-11-14 23:07:02 +0300

    commit 4

--- src/components/Header/Modal/Modal.jsx
+++ src/components/Header/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import style from "./Modal.module.css";
 
@@ -8,19 +8,36 @@ export const Modal = ({active, setActive}) => {
 		setActive(false);
 	}, [])
 	
+	const [user, setUser] = useState({
+		name: '',
+		password: '',
+		email: '',
+	});
+	
+	const onInputChange = (v) => {
+        setUser({ ...user, [v.target.name]: v.target.value });
+    }
 
+	const register = () => {
+		let users = JSON.parse(localStorage.getItem('users'));
+		if (users){
+			localStorage.setItem('users', JSON.stringify([...users, user]))
+		} else {
+			localStorage.setItem('users', JSON.stringify([user]))
+		}
+	}
 
 	return(
 		<div className={active ? `${style.modal} ${style.active}` : style.modal} onClick={() => setActive(false)}>
 			<div className={style.modalContent} onClick={e => e.stopPropagation()}>
 				<h3>Create an account</h3>
 				<label className={style.nameInput}>Name</label>
-				<input id="f" className={style.input} placeholder="Your name" type="text"/>
+				<input id="f" className={style.input} placeholder="Your name" name="name" type="text" onChange={onInputChange} />
 				<label className={style.nameInput}>Email</label>
-				<input className={style.input} placeholder="Your Email"  type="email"/>
+				<input className={style.input} placeholder="Your Email" name='email'  type="email" onChange={onInputChange} />
 				<label className={style.nameInput}>Password</label>
-				<input className={style.input} placeholder="Your password"  type="password"/>
-				<button className={style.buttonRegister}>Register</button>
+				<input className={style.input} placeholder="Your password" name="password"  type="password" onChange={onInputChange} />
+				<button className={style.buttonRegister} onClick={register} >Register</button>
 				<p className={style.uReg}>Do you already have an account?</p>
 				<a href="#">Sing in </a>
 			</div>
--- src/components/Header/ModalAut/ModalAut.jsx
+++ src/components/Header/ModalAut/ModalAut.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import style from "./ModalAut.module.css";
 
@@ -6,15 +6,38 @@ export const ModalAut = ({active, setActive}) => {
 	useEffect(() => {
 		setActive(false);
 	}, [])
+
+	const [user, setUser] = useState({
+		password: '',
+		email: '',
+	});
+	const onInputChange = (v) => {
+        setUser({ ...user, [v.target.name]: v.target.value });
+    }
+
+	const login = () => {
+		let users = JSON.parse(localStorage.getItem('users'));
+		if (!users){
+			alert('юзер не найден'); 
+			return;
+		} 
+		let currentUser = users.find(i => i.email == user.email && i.password == user.password);
+		if (currentUser) {
+			alert('залогин')
+		} else {
+			alert('юзер не найден')
+		}
+	}
+
 	return(
 		<div className={active ? `${style.modal} ${style.active}` : style.modal} onClick={() => setActive(false)}>
 			<div className={style.modalContent} onClick={e => e.stopPropagation()}>
 				<h3>Log in to your account</h3>
 				<label className={style.nameInput}>Email</label>
-				<input className={style.input} placeholder="Your Email"  type="email"/>
+				<input className={style.input} placeholder="Your Email" name="email"  type="email" onChange={onInputChange} value={user.email} />
 				<label className={style.nameInput}>Password</label>
-				<input className={style.input} placeholder="Your password"  type="password"/>
-				<button className={style.buttonRegister}>Register</button>
+				<input className={style.input} placeholder="Your password" name="password" type="password" onChange={onInputChange} value={user.password} />
+				<button className={style.buttonRegister} onClick={login}>Register</button>
 				{/*<div className={style.ques}>*/}
 					<p className={style.uReg}>No account?  <a href="#">Create one</a></p>
 
